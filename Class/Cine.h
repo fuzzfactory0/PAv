333d0d656451334621524099d3ca2442d7061704
Tarica <48918032+TaricaTarica@users.noreply.github.com>
2019-06-03 12:28:48 -0300

    Update Cine.h

--- Class/Cine.h
+++ Class/Cine.h
@@ -1,10 +1,26 @@
-#ifndef SALA
-#define SALA
+#ifndef CINE
+#define CINE
 
 #include "Sala.h"
+#include "Direccion.h"
+#include "dtSala.h"
 
 class Cine{
-
+  private:
+    int id;
+    Direccion direccion;
+  public:
+    Cine();
+    Cine(int,direccion);
+    void setId(int);
+    int getId();
+    void setDireccion(direccion);
+    Direccion getDireccion();
+    dtSala getDtSala();
+    Sala getSalaPelicula();
+    void eliminarPelicula(cine,sala);
+    ~Cine();
 };
 
 #endif;
+
