79f898c5979484f0115b83cb78227fa5e4d807dc
Tarica <48918032+TaricaTarica@users.noreply.github.com>
2019-06-03 12:27:24 -0300

    Update Usuario.h

--- Class/Usuario.h
+++ Class/Usuario.h
@@ -1,8 +1,27 @@
 #ifndef USUARIO
 #define USUARIO
 
-class Usuario{
+#include <string>
+using namespace std;
 
+class Usuario{
+  private:
+    string nickname;
+    string avatarURL;
+    string password;
+  public:
+    Usuario();
+    Usuario(string,string,string);
+    void setNickname(string);
+    string getNickname();
+    void setAvatarURL(string);
+    string getAvatarURL();
+    void setPassword(string);
+    string getPassword();
+    void nombreBanco(Usuario,string, int);
+    void nombreFinanciera(Usuario, string, int);
+    float precioTotal ();
+    ~Usuario();
 };
 
 #endif;
