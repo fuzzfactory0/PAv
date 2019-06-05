ac346bdf8f4c43ea0f4f8bf400932a1c6f576c2f
Tarica <48918032+TaricaTarica@users.noreply.github.com>
2019-06-05 12:20:36 -0300

    Create Sala.cpp

---
+++ Class/Sala.cpp
@@ -0,0 +1,29 @@
+#include "Sala.h"
+
+Sala::Sala(){}
+Sala::Sala(int id,int capacidad){
+  this->id = id;
+  this->capacidad = capacidad;
+}
+void Sala::setId(int id){
+  this->id = id;
+}
+int Sala::getId(){
+  return this->id;
+}
+void Sala::setCapacidad(int capacidad){
+  this->capacidad = capacidad;
+}
+int Sala::getCapacidad(){
+  return this->capacidad;
+}
+DtFuncion* Sala::getFuncionesPelicula(){
+
+}
+DtFuncion Sala::getFuncion(){
+
+}
+void Sala::EliminarFuncion(){
+
+}
+Sala::~Sala(){}
