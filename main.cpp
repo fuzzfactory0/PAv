b36da568ad4cd326923d8afcb220ee1f1cb7d57b
Tarica <48918032+TaricaTarica@users.noreply.github.com>
2019-06-05 12:09:39 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,81 @@
 #include "ICtrlUsuario.h"
 #include "Fabrica.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+void menu();
+
 int main(){
-  Fabrica* fab;
-  ICtrlUsuario* iuser;
+  //creación de fabrica e icontroladores
+  Fabrica* fab = Fabrica::getInstancia();
+  ICtrlUsuario* iuser = fab->getICtrlUsuario();
+
+  //Cargar usuarios al sistema
   iuser->cargarUsuarios();
+
+
+  int opcion;
+  menu();
+  cin >> opcion;
   string nickname;
   string password;
-  cout << "nickname: ";
-  cin >> nickname;
-  cout << "\n";
-  iuser->ingresarNickname(nickname);
-  cout << "password: ";
-  cin >> password;
+  while(opcion != 10){
+    switch(opcion){
+      case 1:
+        cout << "nickname: ";
+        cin >> nickname;
+        iuser->ingresarNickname(nickname);
+        cout << "password: ";
+        cin >> password;
 
-  if(iuser->verificarPassword(password)){
-    iuser->iniciarSesion();
-  } 
+        if(iuser->verificarPassword(password)){
+          iuser->iniciarSesion();
+        }
+        else{
+          cout << "El nombre de usuario o la contraseña son incorrectos";
+        }
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      case 10:
+        break;
+      default:
+        cout << "La opción ingresada no es correcta";
+    }
+  }
 
+}
 
+void menu(){
+  system("clear");
+  cout <<"______________________________________________________________________" <<endl;
+  cout <<"******************************* MENU CINE *********************"<< endl;
+  cout <<"1. Iniciar Sesión"<<endl;
+  cout <<"2. Alta Cine"<<endl;
+  cout <<"3. Alta Función"<<endl;
+  cout <<"4. Crear Reserva"<<endl;
+  cout <<"5. Puntuar Película"<<endl;
+  cout <<"6. Comentar Película"<<endl;
+  cout <<"7. Eliminar Película"<<endl;
+  cout <<"8. Ver Información de Película"<<endl;
+  cout <<"9. Ver Comentarios y Puntajes de Película"<<endl;
+  cout <<"10. Salir"<<endl;
+  cout <<"______________________________________________________________________" <<endl;
+  cout <<"OPCION: ";
 }
