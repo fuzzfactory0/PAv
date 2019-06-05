2a2a5f7a4c5a01b4f57b12340cc9e2670f1995f4
Tarica <48918032+TaricaTarica@users.noreply.github.com>
2019-06-05 12:16:40 -0300

    Update CtrlUsuario.cpp

--- Ctrl/CtrlUsuario.cpp
+++ Ctrl/CtrlUsuario.cpp
@@ -10,9 +10,13 @@ void CtrlUsuario::ingresarNickname(string nickname){
 
 bool CtrlUsuario::verificarPassword(string password){
   HandlerUsuario* hU = HandlerUsuario::getInstancia();
-  Usuario* usr = hU->buscarUsuario(this->nickname);
-  if(usr != NULL && usr->getPassword() == password){
-    return true;
+  if (hU->existeUsuario(this->nickname)){
+    Usuario* usr = hU->buscarUsuario(this->nickname);
+    if(usr != NULL && usr->getPassword() == password){
+      return true;
+    }
+    else
+      return false;
   }
   else
     return false;
@@ -24,6 +28,8 @@ void CtrlUsuario::iniciarSesion(){
   Sesion* s = Sesion::getInstancia();
   s->setUsuario(usr);
 }
-void cargarUsuaros(){
-  Usuario("bukowski","1111","password");
+void CtrlUsuario::cargarUsuarios(){
+  HandlerUsuario* hU = HandlerUsuario::getInstancia();
+  Usuario* u = new Usuario("bukowski","1111","password");
+  hU-> addUsuario(u);
 }
