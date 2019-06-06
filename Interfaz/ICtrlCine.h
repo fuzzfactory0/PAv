b107a8087c9554fe2e97e4d3b13349bd4bb4c298
Tarica <48918032+TaricaTarica@users.noreply.github.com>
2019-06-06 13:14:11 -0300

    Update ICtrlCine.h

--- Interfaz/ICtrlCine.h
+++ Interfaz/ICtrlCine.h
@@ -3,15 +3,16 @@
 
 
 #include <string>
+#include <iostream>
+#include "Direccion.h"
 
 using namespace std;
 
 class ICtrlCine{
   public:
-    virtual void ingresarDireccion(string) = 0;
+    virtual void ingresarDireccion(Direccion) = 0;
     virtual void ingresarCapacidad(int) = 0;
     virtual void AltaCine() = 0;
 };
 
 #endif
-
